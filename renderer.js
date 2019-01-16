61f5d834f25aee0b7c9c094019c112508313505e
Ondra Synáček <ondrasynacek@avocode.com>
2019-01-16 17:13:44 +0100

    add more controls for export

--- renderer.js
+++ renderer.js
@@ -5,6 +5,9 @@
 const remote = require('electron').remote
 const fs = require('fs')
 const path = require('path')
+const os = require('os')
+
+let files
 
 let showWebViews = true 
 let base64 = false
@@ -12,60 +15,58 @@ let log = true
 
 const MICRON = 264.58
 
-const generateWebviewButton = document.createElement('button')
-generateWebviewButton.innerText = 'Generate webviews'
+const homeDir = os.homedir()
+const defaultOutputDir = path.join(homeDir, 'Downloads')
 
-const button = document.createElement('button')
-button.innerText = 'Generate PDFs'
+const generateWebviewButton = document.getElementById('webview')
+const button = document.getElementById('pdf')
+const sampleFilesButton = document.getElementById('samples')
+const folderInput = document.getElementById('dir')
+folderInput.value = defaultOutputDir
 
-const blobOption = document.createElement('input')
-blobOption.id = 'blob'
-blobOption.type = 'checkbox'
+const blobOption = document.getElementById('blob')
 blobOption.value = Number(base64)
 blobOption.checked = base64
-const blobLabel = document.createElement('label')
-blobLabel.htmlFor = blobOption.id 
-blobLabel.innerHTML = 'Base 64'
 
-const webviewOption = document.createElement('input')
-webviewOption.id = 'webviewVisibility'
-webviewOption.type = 'checkbox'
+const webviewOption = document.getElementById('webviewVisibility')
 webviewOption.value = Number(showWebViews)
 webviewOption.checked = showWebViews 
-const webviewLabel = document.createElement('label')
-webviewLabel.htmlFor = webviewOption.id 
-webviewLabel.innerText = 'Webviews visible' 
 
-const logOption = document.createElement('input')
-logOption.id = 'log'
-logOption.type = 'checkbox'
+const logOption = document.getElementById('log')
 logOption.value = Number(log)
 logOption.checked = log
-const logLabel = document.createElement('label')
-logLabel.htmlFor = logOption.id
-logLabel.innerHTML = 'Logs'
 
-const generateWebview = (inPath) => {
+const dropArea = document.getElementById('droparea')
+const counter = document.getElementById('counter')
+
+const resetLoadedFiles = () => {
+    files = null
+}
+
+const round = number => Math.ceil(number)
+
+const generateWebview = (file) => {
     Array.from(document.getElementsByTagName('webview')).forEach((w) => {
         w.remove()
     })
     Array.from(document.getElementsByTagName('table')).forEach((t) => {
         t.remove()
     })
+    const filePath = file.path
     const webview = document.createElement('webview')
-    webview.src = inPath
+    webview.src = filePath 
     if (!showWebViews) {
         webview.style.visibility = 'hidden'
     }
 
     const image = new Image()
-    image.src = inPath
+    image.src = filePath
     image.onload = () => {
-        const imageName = inPath
+        const imageName = filePath
         const imageWidth = image.naturalWidth
         const imageHeight = image.naturalHeight
-        const pdfWidth = imageWidth * MICRON
-        const pdfHeight = imageHeight * MICRON
+        const pdfWidth = round(imageWidth * MICRON)
+        const pdfHeight = round(imageHeight * MICRON)
 
         webview.style.width = `${imageWidth}px`
         webview.style.height = `${imageHeight}px`
@@ -113,15 +114,20 @@ const generateWebview = (inPath) => {
 }
 
 const generatePdf = (webview) => {
+    const outDir = folderInput.value
+
+    if (!outDir || !fs.existsSync(outDir)) {
+        alert('Enter valid path')
+    }
+
     const url = new URL(webview.src)
     const pathName = url.pathname.split('/')
     const fileName = pathName[pathName.length - 1]
     const name = fileName.replace(/\..*/, '')
-    console.log(`SIZES -> width: ${webview.clientWidth * MICRON}; height: ${webview.clientHeight * MICRON}`)
     webview.getWebContents().printToPDF({
         pageSize: {
-            width: webview.clientWidth * MICRON,
-            height: webview.clientHeight * MICRON
+            width: round(webview.clientWidth * MICRON),
+            height: round(webview.clientHeight * MICRON)
         },
         marginsType: 1,
         printBackground: false,
@@ -129,7 +135,7 @@ const generatePdf = (webview) => {
         if (error) { throw error }
         console.log('data', data)
 
-        fs.writeFile(`${path.join('/', 'private', 'tmp', name)}.pdf`, data, (error) => {
+        fs.writeFile(`${path.join(outDir, name)}.pdf`, data, (error) => {
             if (error) { throw error }
             console.log('Write PDF successfully.')
         })
@@ -140,21 +146,25 @@ const fetchWebviews = () => {
     return document.getElementsByTagName('webview')
 }
 
-const paths = [
-    './rectangle3.svg',
-    './rectangle2.svg',
-    './rectangle.svg',
-    './square.svg',
-    './sketch.svg',
-    './circle_png.png',
-    './circle.svg',
-    './cartman_png.png',
-    './cartman.svg',
-    './kyle.svg'
+const sampleFilenames = [
+    'rectangle3.svg',
+    'rectangle2.svg',
+    'rectangle.svg',
+    'square.svg',
+    'sketch.svg',
+    'circle_png.png',
+    'circle.svg',
+    'cartman_png.png',
+    'cartman.svg',
+    'kyle.svg'
 ]
 
 generateWebviewButton.addEventListener('click', () => {
-    paths.forEach(generateWebview)
+    if (!files || files.length < 0) {
+        alert('Missing files')
+        return
+    }
+    Array.from(files).forEach(generateWebview)
 })
 
 button.addEventListener('click', () => {
@@ -166,6 +176,14 @@ button.addEventListener('click', () => {
 
     Array.from(webviews).forEach((webview) => {
         generatePdf(webview)
+    })
+})
+
+sampleFilesButton.addEventListener('click', () => {
+    resetLoadedFiles()
+    updateCounter()
+    sampleFilenames.forEach((fileName) => {
+        generateWebview({ path: path.join(__dirname, fileName) })
     })
 })
 
@@ -187,15 +205,49 @@ logOption.addEventListener('click', (el) => {
     el.checked = log 
 })
 
+const handleDnd = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+}
 
-document.body.appendChild(document.createElement('br'))
-document.body.appendChild(generateWebviewButton)
-document.body.appendChild(button)
-document.body.appendChild(blobOption)
-document.body.appendChild(blobLabel)
-document.body.appendChild(webviewOption)
-document.body.appendChild(webviewLabel)
-document.body.appendChild(logOption)
-document.body.appendChild(logLabel)
-document.body.appendChild(document.createElement('br'))
-document.body.appendChild(document.createElement('br'))
+const highlight = () => {
+    dropArea.classList.add('highlight')
+}
+
+const unhighlight = () => {
+    dropArea.classList.remove('highlight')
+}
+
+const handleDragEnter = (e) => {
+    handleDnd(e)
+    highlight()
+}
+
+const handleDragOver = (e) => {
+    handleDnd(e)
+    highlight()
+}
+
+const handleDragLeave = (e) => {
+    handleDnd(e)
+    unhighlight()
+}
+
+const handleDrop = (e) => {
+    resetLoadedFiles()
+    handleDnd(e)
+    unhighlight()
+
+    const transfer = e.dataTransfer
+    files = transfer.files
+    updateCounter()
+}
+
+const updateCounter = () => {
+    counter.innerHTML = `${files ? files.length : '0'} items`
+}
+
+dropArea.addEventListener('dragenter', handleDragEnter, false)
+dropArea.addEventListener('dragleave', handleDragLeave, false)
+dropArea.addEventListener('dragover', handleDragOver, false)
+dropArea.addEventListener('drop', handleDrop, false)
